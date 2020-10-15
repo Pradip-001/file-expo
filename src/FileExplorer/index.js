09eb495223186ecd5af190250fbf4d9cafafa4e3
Pradipkumar <karnavatpradip12345@gmail.com>
2020-10-15 11:22:31 +0530

    added border and radius

--- src/FileExplorer/index.js
+++ src/FileExplorer/index.js
@@ -1,27 +1,29 @@
-import React from "react";
-import { Folder } from "./Folder";
+import React from 'react'
+import { Folder } from './Folder'
 
 export function FileExplorer({ content, depth, ...props }) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "248px",
-        marginLeft: depth + "px",
-      }}
-    >
-      {Object.keys(content).map(function (key, index) {
-        if (typeof content[key] === "string") {
-          return content[key];
-        }
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '248px',
+                marginLeft: depth + 'px',
+                border: '1px solid red',
+                borderRadius: '5px',
+            }}
+        >
+            {Object.keys(content).map(function (key, index) {
+                if (typeof content[key] === 'string') {
+                    return content[key]
+                }
 
-        return (
-          <Folder name={key}>
-            <FileExplorer content={content[key]} depth={depth} />
-          </Folder>
-        );
-      })}
-    </div>
-  );
+                return (
+                    <Folder name={key}>
+                        <FileExplorer content={content[key]} depth={depth} />
+                    </Folder>
+                )
+            })}
+        </div>
+    )
 }
